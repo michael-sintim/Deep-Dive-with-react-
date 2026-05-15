2a469f688c32fb210d3897c6dd603205943c0e72
Michael Sintim <michaelsintim673@gmail.com>
2026-05-15 08:32:20 +0000

    almost done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -49,18 +49,23 @@ setChatMessages(
 
 
 return(
-<div className="flex justify-center m-6">
+    <>
+<div className="flex justify-center m-5">
 
  <input type="text" placeholder="Send a message to the Chatbot" className="border"
  value={inputText} size='30' onKeyDown={handlekeydown}
  onChange={saveText}
- className='w-[500px] border rounded-lg h-10 text-center p-2 mx-4'
+ className='w-[500px] border rounded-lg h-10 text-left p-5 mx-4'
  
  />
  <button onClick={SendMessage} className="bg-blue-600 p-2 rounded-lg text-white px-5 hover:cursor-pointer">Send</button>
   
 </  div>
-
+<div className="flex justify-center">
+    
+      <p className="text-gray-500">Welcome to the chatbot project! Send a message using the textbox above</p>
+</div>
+</>
 )
 }
 
